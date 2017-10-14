b8eacd35b3cb62e8f95a6ef6a33a417d8782aa51
jhonychamorro19 <j.chamorro.reinoso@outlook.com>
2017-10-13 21:47:03 -0500

    meDijisteAlgo.js

--- Construccion de Bots simples/meDijisteAlgo.js
+++ Construccion de Bots simples/meDijisteAlgo.js
@@ -3,8 +3,9 @@ var builder = require('botbuilder');
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog ('/',[
-    function(session) {
-        session.send('!Hello world!');
+bot.dialog('/', [
+    function (session) {
+        let msj = session.message.text;
+        session.send(`Me dijiste: ${msj}`);
     }
 ]);
