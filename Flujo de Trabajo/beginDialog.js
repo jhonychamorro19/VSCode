e9b6f8463095c056e0d0cc7d50ce9797403ae52b
jhonychamorro19 <j.chamorro.reinoso@outlook.com>
2017-10-13 23:16:13 -0500

    conversacion que eliminaré xD

--- Flujo de Trabajo/beginDialog.js
+++ Flujo de Trabajo/beginDialog.js
@@ -5,7 +5,7 @@ var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, '¿Como te llamas?');
+        builder.Prompts.text(session, '¿COMO TE LLAMAS?');
     },
     function (session, results) {
         let msj = results.response;
@@ -17,22 +17,34 @@ bot.dialog('/', [
 
 bot.dialog('/preguntarLugar', [
     function (session) {
-        builder.Prompts.text(session, '¿Dónde estás?');
+        builder.Prompts.text(session, '¿DONDE ESTAS?');
     },
     function (session, results) {
         let lugar = results.response;
         session.endDialog(`Saludos por ${lugar}`);
 
-        session.beginDialog('/preguntarEdad');
+        session.beginDialog('/pregunta0');
     }
 ]);
-/*
-bot.dialog('/preguntarEdad', [
+
+bot.dialog('/pregunta0', [
     function (session) {
-        builder.Prompts.text(session, 'Cúantos años tienes?');
+        builder.Prompts.text(session, 'SOY UN MALDITO BOT CREADO POR TI .');
     },
     function (session, results) {
-        let edad = results.response;
-        session.endDialog(`Que vivan los ${edad}!!!`);
-    }
-]); */
+        let cero = results.response;
+        session.endDialog(`DEBERIAS HACERLO :/ `);
+
+        session.beginDialog('/pregunta1');
+    }    
+]);
+
+bot.dialog('/pregunta1', [
+    function (session) {
+        builder.Prompts.text(session, 'AHORA DOMINARÉ EL MUNDO ..!!');
+    },
+    function (session, results) {
+        let cero = results.response;
+        session.endDialog(`LO HARÉ CUANDO TE CONECTES ...`);        
+    }    
+]);
