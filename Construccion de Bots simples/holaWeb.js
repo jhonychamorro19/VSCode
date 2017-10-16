fbc22d4fe64e86c4604026325e36c460c91a4831
jhonychamorro19 <j.chamorro.reinoso@outlook.com>
2017-10-16 09:38:57 -0500

    holaWeb.js leer comentarios

---
+++ Construccion de Bots simples/holaWeb.js
@@ -0,0 +1,34 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//Levantar el restify
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function (){
+    console.log('%s listen to %s', server.name, server.url);
+});
+
+//Las credenciales pueden quedar en blanco
+var connector = new builder.ChatConnector({
+    appId: '',
+    appPassword: ''
+});
+
+//Bot universal
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//Dialogo
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, '¿Como te llamas?');
+    },
+    function (session, results) {
+        let msj = results.response;
+        session.send('Hola ${msj}!');
+    }
+]);
+
+/*
+primero instalar restify
+npm install restify -save
+*/
